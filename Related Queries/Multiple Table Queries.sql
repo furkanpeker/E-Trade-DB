16595e02ba27e16b4e29c78e89f684fd6cf5ce39
Furkan PEKER <73311880+furkanpeker@users.noreply.github.com>
2022-01-24 23:04:12 +0300

    Add files via upload

---
+++ Related Queries/Multiple Table Queries.sql
@@ -0,0 +1,112 @@
+﻿/* Kendisinden verilen toplam sipariş miktarı 5'ten fazla olan ürünlerin ürün kodu ve verilen toplam sipariş miktarını listeleyen sorgu: */
+select U.urun_id as urun_kodu, sum(S.miktar) as toplam_Siparis_Miktari
+from [Urun-SiparisDetay] US
+join Urunler U on US.urun_id=U.urun_id
+join SiparisDetay S on US.siparisDetay_id=S.siparisDetay_id
+group by U.urun_id having sum(S.miktar) > 5
+
+
+/* Ürün ID si 3 ve 4 olanları, aynı zamanda 5 ve 6 olanları azalan olarak sıralayıp ve birleştirip getiren sorgu:  */
+select a.urun_id
+from
+(
+    select top 10 urun_id 
+    from Urunler
+    where urun_id in(3, 4)
+    order by urun_adi desc
+) a
+union
+select b.urun_id
+from
+(
+    select top 10 urun_id 
+    from Urunler
+    where urun_id in(5, 6)
+    order by urun_adi desc
+) b
+
+
+/* 2021 yılında ya da her hangi bir yılın 12'nci ayında sipariş veren üyeleri ve bu üyelerin siparişlerinin toplam ne kadar tuttuğunu veren sorgu:  */
+select U.uye_ad + ' ' + U.uye_soyad as [Üyenin Ad ve Soyadı], S.toplamTutar 
+from Siparisler S, Uyeler U 
+where U.uye_id = S.uye_id and (year(S.siparis_tarihi) = 2021 or MONTH(S.siparis_tarihi)=12)
+
+/* 2021 yılında sipariş veren müşterilerin id lerini ve telefonlarını listeleyen sorgu:  */
+select M.musteri_id [musteri_id], M.telefonNO telefon 
+from Siparisler S inner join Musteri M
+on S.musteri_id = M.musteri_id
+where year(S.siparis_tarihi) = 2021
+
+/* 2021 yılında id si 9 olan üyenin adının ilk harfini, verdiği siparişin id sini ve bu üyenin vermiş olduğu siparişin ödeme şeklini listeleyen sorgu: */
+select left(U.uye_ad, 1) [Üye adı baş harfi], S.siparis_id [sipariş id], O.odeme_turu [Odeme şekli] 
+from Siparisler S
+full join Uyeler U on S.uye_id = U.uye_id
+inner join Odemeler O on S.odeme_id = O.odeme_id
+where U.uye_id = 9 and datepart(year, S.siparis_tarihi) = 2021
+
+/* Siparişler tablosu ile müşteri tablosunun birleşimini veren sorgu: */
+select * from Siparisler AS S JOIN Musteri AS M
+ON S.musteri_id = M.musteri_id
+ORDER BY S.siparis_tarihi DESC
+
+/* Müşteri adına göre azalan sırada; müşteri adını ve o müşterinin verdiği siparişleri listeleyen sorgu: */
+select M.sirket_ad [Müşteri şirkertin adı], S.siparis_id
+from Musteri M left join Siparisler S
+on S.musteri_id = M.musteri_id
+order by M.sirket_ad
+
+/* İsminde 't' geçen bir kargo şirketinin  siparişlerinin verildiği adresin metni ve müşterisinin adını veren sorgu: */
+select A.adres_metni, M.sirket_ad [Müşteri Adı]
+from Siparisler S
+join KargoSirketi KS on KS.kargoSirketi_id = S.kargoSirketi_id
+join Adresler as A on A.adres_id = S.adres_id
+join Musteri M on M.musteri_id = S.musteri_id
+where KS.sirketAd = '%t%'
+
+/* 2021 yılında en pahalı siparişi veren üyenin adı ve soyadını ve verdiği siparişin toplam tutarını veren sorgu: */
+select top 1 U.uye_ad, U.uye_soyad, S.toplamTutar                                                                                                                                      
+from Siparisler S
+join Uyeler U on U.uye_id = S.uye_id
+order by S.toplamTutar desc
+
+/* Bir siparişin; sipariş id, o siparişi veren müşterinin (şirket) adını ve verildiği yılı veren sorgu: */
+select S.siparis_id, M.sirket_ad, year(S.siparis_tarihi) [Sipariş Yılı] from Siparisler as S, Musteri as M
+where S.musteri_id = M.musteri_id 
+order by S.siparis_tarihi desc
+
+/* Hangi müşteriler ve hangi üyeler hangi yılda sipariş vermiş? bunu veren sorgu: */
+select year(S.siparis_tarihi) [Sipariş Yılı], M.sirket_ad, U.uye_ad + ' ' + U.uye_soyad [uye ad-soyad]
+from Siparisler S
+Right Join Musteri M on S.musteri_id = M.musteri_id
+Right Join Uyeler U on S.uye_id = U.uye_id
+
+/* Ayın 1.gününde sipariş veren müşterilerin hangi şehirde yaşadığını gösteren sorgu: */
+select S.musteri_id [Müşteri No], Sh.sehir_adi [Şehir]
+from Adresler A
+join Siparisler S on S.adres_id = A.adres_id
+join Sehirler Sh on Sh.sehir_id = A.sehir_id
+where datepart(day,S.siparis_tarihi) = 1
+
+/* Ödeme şekli peşin olan kargo şirketinin kargo araçlarının sayısını veren sorgu: */
+select count(K.kargoAraci) [Kargo Aracı Sayısı]
+from KargoSirketi KS 
+left join Kargolar K on KS.kargoSirketi_id = K.kargoSirketi_id
+join Odemeler O on O.odeme_id = KS.odeme_id
+where KS.odeme_id = 2
+
+/* 0216 349 84 55 telefon numarasına sahip olan tedarikçi şirketin tedarik ettiği ürünlerin isim - birim fiyat listesi: */
+select U.urun_adi [isim], U.birimFiyat [birim fiyat]
+from [Urun-TedarikciSirket] UT 
+join Urunler U on UT.urun_id = U.urun_id
+join TedarikciSirket T on T.[t-sirket_id] = UT.[t-sirket_id]
+where T.telefonNO = '0216 349 84 55'
+
+/* Siparişler tablosu ile müşteri tablosunun kartezyen çarpımını veren sorgu: */
+select M.musteri_id, S.siparis_id
+from Musteri M cross join Siparisler S
+order by M.musteri_id
+
+
+
+
+
---
+++ Related Queries/Single Table Queries.sql
@@ -0,0 +1,79 @@
+﻿/* Odemelerin tarihlerine göre ortalamasını, tarihe göre azalan sırada listeleyen sorgu: */
+select tarih odeme_tarihi, avg(odemeToplam) as Ort_Odeme
+from Odemeler 
+group by tarih 
+order by tarih desc
+
+/* Her bir tarihte kaç fatura basıldı? */
+select tarih faturaTarihi, count(fatura_id) basilanFaturaAdedi
+from Faturalar 
+group by tarih 
+
+/* Müşterilerin sistemde oluşturulma / sisteme ilk dahil edilme tarihlerine göre sayısını veren sorgu: */
+select count(musteri_id) as musteri_sayisi, musteri_olusturulma_tarihi as [musteri olusturulma tarihi]
+from Musteriler 
+group by musteri_olusturulma_tarihi
+order by count(*) desc
+
+/* birim fiyatı 10 birim üstünde olan ürünlerin adedini, kendisinden temin edilen tedarikçi şirkete göre gruplandırıp adedi artan sırada listeleyen sorgu: */
+select tedarikci_id [Tedarikçi Şirket], count(*) [ürün adedi]
+from Urunler
+where birimFiyat > 10
+group by tedarikci_id
+order by count(*)
+
+/* Düşünün ki tedarikçi şirketlerden biri, size tedarik ettiği tüm mallarına %20 zam getiriyor. O halde sadece bu şirketten tedarik edilen malların zamdan önce ve sonraki durumlarını birim fiyatlarına göre azalan olarak gösteren sorgu: */
+select urun_adi, birimFiyat, (birimFiyat + birimFiyat*20/100) as  zamliBirimFiyat 
+from Urunler 
+where tedarikci_id=5
+order by birimFiyat desc
+
+/* Verilen siparişlerin hangi müşteri (id olarak) tarafından ve toplam kaç paraya verdiğinin hepsini bir sütunda gösteren sorgu: */
+select convert(nvarchar(10), musteri_id) + ': ' + convert(nvarchar(12), convert(int, toplamTutar)) as [Musteri ve toplam siparis tutarı]
+from Siparisler
+
+/* E-ticaret sitesinin pazara çıkardığı ürünlerin birim fiyatlarının ortalamasını veren sorgu: */
+select avg(birimFiyat) [Ortalama fiyat] 
+from Urunler
+
+/* Üyeler tablosundaki üyelerin soyadlarının son iki harfini veren sorgu: */
+select right(uye_soyad, 2) [son iki harf]
+from Uyeler
+
+/* Aynı cinsiyette olan Kontakları, kontak ünvanlarına göre sayılarını kontak ünvanlarına göre azalan sırada veren sorgu: */
+select count(kontak_id) as [kontank sayısı], cinsiyet, kontak_unvan as [ünvan]
+from Kontaklar
+group by cinsiyet, kontak_unvan
+order by kontak_unvan
+
+/* her bir ürünün birim fiyatına %10 zam getirip tam sayı değer olacak şekilde getiren sorgu: */
+select kategori_id, convert(int, birimFiyat+birimFiyat*10/100) as [%10 zam uygulanmış ve tam sayı olmuş birim fiyat]
+from Urunler
+
+/* Üyeler tablosundaki üyelerin herbirinin ne kadar süredir üye olduklarını veren sorgu: */
+select uye_ad [Üyenin Adı], abs(datediff(year, getDate(), uye_kayit_tarih)) [Ne zamandır üye? (yıl)]
+from Uyeler
+
+/* Birden fazla tedarikçi tarafından temin edilen ürünlerin id'lerini ve ne kadar tedarikçi tarafından temin edildiklerini veren sorgu: */
+select urun_id, count(*) as [tedarikçi adet]
+from [Urun-TedarikciSirket]
+group by urun_id having count(*) > 1
+
+/* Veri tabanında; birden fazla şehire sahip olan ülkelerin id'lerini ve kaç şehre sahip olduklarını veren sorgu: */
+select ulke_id, count(*) as [sahip olduğu şehir sayısı]
+from Sehirler
+group by ulke_id having count(*) > 1
+
+/* Odeme türleri ve tarihlerine göre, ödememlerden toplam en fazla tutan ilk 2 ödemenin türü ve tarihini ve ne kadar tuttuğunu veren sorgu: */
+select top 2 odeme_turu [ödeme türü], tarih, odemeToplam [Ne kadar tuttu?]
+from Odemeler
+group by odeme_turu, tarih, odemeToplam
+order by odemeToplam desc
+
+/* Aynı kargo şirketi ile taşınan ve durumları da halen 'beklemede' olan siparişlerin ödeme yöntemlerine göre en yüksek ve en düşük toplam tutarlarını ödeme yöntemine göre artan sırada getiren sorgu: */
+select kargoSirketi_id, odeme_id, max(toplamTutar) as [maksimum toplam tutar], min(toplamTutar) as [minimum toplam tutar] 
+from Siparisler 
+where durum='beklemede'
+group by kargoSirketi_id, odeme_id 
+order by odeme_id
+
